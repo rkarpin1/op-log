4a22adbd48a8f160b1b12e399652ffc3e5fc56d2
Robert Karpiński <rkarpin1@gmail.com>
2026-08-25 23:45:44 +0200

    Fold the one-caller LogDefinition::clean_up into OpLogWorker::clean_up

--- src/clean_up.rs
+++ src/clean_up.rs
@@ -2,24 +2,20 @@
 //   Copyright 2024-2025 (c) Robert Karpiński
 // -------------------------------------------------------------------------------------------------
 
-use crate::{LogDefinition, OpLogWorker};
+use crate::OpLogWorker;
 use std::time::Duration;
 
 impl OpLogWorker {
     pub(crate) fn clean_up(&mut self) {
-        self.definitions.values_mut().for_each(|d| d.clean_up());
+        self.definitions.values_mut().for_each(|d| {
+            d.files.retain(|_, f| {
+                !f.logs.is_empty() || f.last_time_use.elapsed() < Duration::from_secs(10 * 60)
+            })
+        });
 
         self.definitions.retain(|_, d| {
             !d.auto_remove_definition ||
                 (!d.files.is_empty() || d.last_time_use.elapsed() < Duration::from_secs(10 * 60))
-        });
-    }
-}
-
-impl LogDefinition {
-    fn clean_up(&mut self) {
-        self.files.retain(|_, f| {
-            !f.logs.is_empty() || f.last_time_use.elapsed() < Duration::from_secs(10 * 60)
         });
     }
 }
